7a676f312d8ace00a1d29497a346326271a4ef23
Vinay Kumar Singh <vin05_singh@yahoo.com>
2020-08-30 20:25:15 +0530

    initil commit

--- web/src/assets/js/personalize.js
+++ web/src/assets/js/personalize.js
@@ -1,11 +1,4 @@
-(function() {
-    const cage = 3*365*24*60*60;
-    document.cookie = "visited=true; path=/; max-age=" + cage; 
-    const article = document.querySelector(".data-article");
-    const challenge = document.querySelector(".data-challenge");
-    const quiz = document.querySelector(".data-quiz");
-
-    if (article || challenge || quiz) {
-        // update ids in the cookie 
-    }
+(() => {
+  const cage = 3 * 365 * 24 * 60 * 60;
+  document.cookie = `visited=true; path=/; max-age=${cage}`;
 })();
